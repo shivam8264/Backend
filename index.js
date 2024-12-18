fbb1a69496717d161fcf5322f3e3471b85936e9c
shivam8264 <shivamsy05@gmail.com>
2024-12-18 13:52:11 +0530

    Initial commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
     cors: {
-        origin: 'https://newspluslive.netlify.app',
+        origin: process.env.REACT_APP_FRONTEND_URL,
         methods: ['GET', 'POST']
     }
 });
