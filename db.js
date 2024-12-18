c77a6934e2b0cc8f2082bb5c6b06984a301e682d
Shivam Yadav <118033383+shivam8264@users.noreply.github.com>
2024-12-18 14:07:47 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const mongoURI="mongodb+srv://shivamsy8264:shivam8764@newsplus.an838.mongodb.net/NewsBook";
+const mongoURI="mongodb+srv://shivamsy8264:shivam8764@newsplus.an838.mongodb.net/NewsBook?retryWrites=true&w=majority";
 const connectToMongo = async () => {
     try {
         await mongoose.connect(mongoURI, {
